32cd970c6f2e669fd22d196d263e91f5892f9003
Daniel Combiths <danielcombiths@hotmail.com>
2015-06-17 09:24:48 -0400

    Update 2015-06-17T13:24:46.422Z

--- scripts/app-dff52898.js
+++ scripts/app-dff52898.js
@@ -22,6 +22,93 @@ angular.module('sv')
     $scope.date = new Date();
   }]);
 
+'use strict';
+
+angular.module('sv')
+  .controller('MainCtrl', ["$scope", "GithubAPI", function ($scope, GithubAPI) {
+	
+    $scope.user = {'username': '', 'repos':[], 'branches':[], 'currentRepo':'', 'currentBranch': 'master', 'currentCommits':[]}
+    $scope.usernameChange = function(){
+        GithubAPI.getRepos($scope.user.username).then(function(data){
+            $scope.user.repos = [];
+            angular.forEach(data, function(repo){
+                $scope.user.repos.push(repo.full_name.split("/").pop());
+            });
+        });
+    }
+    
+    $scope.repoChanged = function(){
+        $scope.user.branches = [];
+        GithubAPI.getBranches($scope.user.username, $scope.user.currentRepo).then(function(data){
+            angular.forEach(data, function(branch){
+                $scope.user.branches.push(branch.name);
+            });
+        });
+    }
+
+
+    $scope.branchChanged = function(){
+        GithubAPI.getCommits($scope.user.username, $scope.user.currentRepo, $scope.user.currentBranch).then(function(data){
+            $scope.user.currentCommits = [];
+			//var dbClient = new Dropbox.Client({ key: 'ul8h8jpx9o164n1'});
+			var dbClient = new Dropbox.Client({ key: '4nl4o8v9y9wqv1i' });
+			dbClient.authDriver(new Dropbox.AuthDriver.Popup({ receiverUrl:  'https://ronlaflamme.github.io/sv/oauth_receiver.html' }));
+			dbClient.authenticate(function(authError){
+				if(authError || !dbClient.isAuthenticated()){
+					alert("Cannot login to Dropbox!");
+				}
+				else{
+					//angular.forEach(data, function(commit, key){
+						//if(key > 3){return;}
+						var commit = data[0];
+						GithubAPI.getCommit($scope.user.username, $scope.user.currentRepo,
+											 commit.sha).then(function(commitInfo){
+												 
+						if(commitInfo.hasOwnProperty("files") &&
+							commitInfo.files.length > 0){	
+							
+							var filename = $scope.user.currentRepo + '/' + commitInfo.files[0].filename;
+							
+							dbClient.history(filename, function(error, revisions){
+								var hostID;
+								if(error){
+									hostID = error.responseText;
+								}
+								else{
+									hostID = revisions[0]["host_id"];
+								}
+								
+								$scope.user.currentCommits.push({
+									'timestamp': commit.commit.committer.date, 
+									'hostId':  hostID,
+									'commit': commit.sha});
+								
+							});
+						}
+						});
+					/*	break;
+					});*/
+				}
+			});		
+		});
+    }
+		
+	$scope.saveToDropboxClick = function(){
+		//create byte stream
+		angular.forEach($scope.user.currentCommits, function(commit){
+			//add to byte stream
+		});
+		
+		//store to Dropbox
+		/*client.writeFile("hello_world.txt", "Hello, world!\n", function(error, stat) {
+		if (error) {
+			return showError(error);  // Something went wrong.
+		}
+		  alert("File saved as revision " + stat.versionTag);
+		});*/
+	}
+  }]);
+
 (function (){
     angular.module('sv').factory('GithubAPI', GithubAPI);
     GithubAPI.$inject = ['$http', '$q'];
@@ -86,91 +173,5 @@ angular.module('sv')
         };
     }
 })();
-'use strict';
-
-angular.module('sv')
-  .controller('MainCtrl', ["$scope", "GithubAPI", function ($scope, GithubAPI) {
-	
-    $scope.user = {'username': '', 'repos':[], 'branches':[], 'currentRepo':'', 'currentBranch': 'master', 'currentCommits':[]}
-    $scope.usernameChange = function(){
-        GithubAPI.getRepos($scope.user.username).then(function(data){
-            $scope.user.repos = [];
-            angular.forEach(data, function(repo){
-                $scope.user.repos.push(repo.full_name.split("/").pop());
-            });
-        });
-    }
-    
-    $scope.repoChanged = function(){
-        $scope.user.branches = [];
-        GithubAPI.getBranches($scope.user.username, $scope.user.currentRepo).then(function(data){
-            angular.forEach(data, function(branch){
-                $scope.user.branches.push(branch.name);
-            });
-        });
-    }
-
-
-    $scope.branchChanged = function(){
-        GithubAPI.getCommits($scope.user.username, $scope.user.currentRepo, $scope.user.currentBranch).then(function(data){
-            $scope.user.currentCommits = [];
-			//var dbClient = new Dropbox.Client({ key: 'ul8h8jpx9o164n1'});
-			var dbClient = new Dropbox.Client({ key: '4nl4o8v9y9wqv1i' });
-			dbClient.authDriver(new Dropbox.AuthDriver.Popup({ receiverUrl:  'https://ronlaflamme.github.io/sv/oauth_receiver.html' }));
-			dbClient.authenticate(function(authError){
-				if(authError || !dbClient.isAuthenticated()){
-					alert("Cannot login to Dropbox!");
-				}
-				else{
-					//angular.forEach(data, function(commit, key){
-						//if(key > 3){return;}
-						GithubAPI.getCommit($scope.user.username, $scope.user.currentRepo,
-											 commit.sha).then(function(commitInfo){
-												 
-						if(commitInfo.hasOwnProperty("files") &&
-							commitInfo.files.length > 0){	
-							
-							var filename = $scope.user.currentRepo + '/' + commitInfo.files[0].filename;
-							
-							dbClient.history(filename, function(error, revisions){
-								var hostID;
-								if(error){
-									hostID = error.responseText;
-								}
-								else{
-									hostID = revisions[0]["host_id"];
-								}
-								
-								$scope.user.currentCommits.push({
-									'timestamp': commit.commit.committer.date, 
-									'hostId':  hostID,
-									'commit': commit.sha});
-								
-							});
-						}
-						});
-					/*	break;
-					});*/
-				}
-			});		
-		});
-    }
-		
-	$scope.saveToDropboxClick = function(){
-		//create byte stream
-		angular.forEach($scope.user.currentCommits, function(commit){
-			//add to byte stream
-		});
-		
-		//store to Dropbox
-		/*client.writeFile("hello_world.txt", "Hello, world!\n", function(error, stat) {
-		if (error) {
-			return showError(error);  // Something went wrong.
-		}
-		  alert("File saved as revision " + stat.versionTag);
-		});*/
-	}
-  }]);
-
 angular.module("sv").run(["$templateCache", function($templateCache) {$templateCache.put("app/main/main.html","<div class=\"container\"><div ng-include=\"\'app/components/navbar/navbar.html\'\"></div><div class=\"jumbotron text-center\"><h1>LaFlamme\'s Github</h1><p class=\"lead\">Commit Audit Log</p><input type=\"text\" ng-model=\"user.username\" placeholder=\"user name\"> <input type=\"button\" ng-click=\"usernameChange()\" value=\"Fetch Repos\"><select ng-change=\"repoChanged()\" ng-model=\"user.currentRepo\" ng-options=\"o as o for o in user.repos\"></select><select ng-change=\"branchChanged()\" ng-model=\"user.currentBranch\" ng-options=\"o as o for o in user.branches\"></select></div><div class=\"row\"><table class=\"table table-condensed table-striped table-condensed\"><thead><tr><th>Time Stamp</th><th>Host ID</th><th>Commit</th></tr></thead><tbody><tr ng-repeat=\"commit in user.currentCommits track by $index\"><td>{{commit.timestamp}}</td><td>{{commit.hostId}}</td><td>{{commit.commit}}</td></tr></tbody></table></div><div><input type=\"button\" ng-click=\"saveToDropboxClick()\" value=\"Save to Dropbox\"></div></div>");
 $templateCache.put("app/components/navbar/navbar.html","<nav class=\"navbar navbar-static-top navbar-inverse\" ng-controller=\"NavbarCtrl\"><div class=\"container-fluid\"><div class=\"navbar-header\"><a class=\"navbar-brand\" href=\"/\"><span class=\"glyphicon glyphicon-home\"></span> SV</a></div><div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-6\"><ul class=\"nav navbar-nav\"><li class=\"active\"><a ng-href=\"#\">Home</a></li><li><a ng-href=\"#\">About</a></li><li><a ng-href=\"#\">Contact</a></li></ul><ul class=\"nav navbar-nav navbar-right\"><li>Current date: {{ date | date:\'yyyy-MM-dd\' }}</li></ul></div></div></nav>");}]);
