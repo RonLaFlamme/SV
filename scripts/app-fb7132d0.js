c2aa5b0639b22f162d71943177fe6c1956cce35e
Daniel Combiths <danielcombiths@hotmail.com>
2015-06-18 00:47:36 -0400

    Update 2015-06-18T04:47:34.346Z

--- scripts/app-fb7132d0.js
+++ scripts/app-fb7132d0.js
@@ -30,7 +30,7 @@ angular.module('sv')
         var getRepos = function(username)
         {
             var deferred = $q.defer();
-            $http.get(baseURL+'/users/'+username+'/repos').
+            $http.get(baseURL+'/users/'+username+'/repos?client_id=806e681c8e84253e21ee&client_secret=8683fce7615e8304aba4fdb9b0659832659d1cbe').
                 success(function (data, status, headers, config) {
                     deferred.resolve(data);
                 }).
